6d0e99e9ad059118b0dd5a9047d5bf0c1b9c12b2
Asif2501 <dhanushrajavel24@gmail.com>
2026-02-18 16:59:56 +0530

    zone mark bugs fixing

--- src/api-endpoints/ApiUrls.ts
+++ src/api-endpoints/ApiUrls.ts
@@ -3,34 +3,34 @@ const baseUrl =
   "https://api.itfixer199.com";
 
 const login = `${baseUrl}/api/login`;
-const allUsers=`${baseUrl}/api/user/all`;
-const allStols =`${baseUrl}/api/slot/all`;
-const createUser=`${baseUrl}/api/user`;
-const createSlot=`${baseUrl}/api/slot/create`;
-const slot=`${baseUrl}/api/slot`;
-const createZone =`${baseUrl}/api/zone/create`;
-const zone=`${baseUrl}/api/zone`;
-const allZone=`${baseUrl}/api/zone/all`;
+const allUsers = `${baseUrl}/api/user/all`;
+const allStols = `${baseUrl}/api/slot/all`;
+const createUser = `${baseUrl}/api/user`;
+const createSlot = `${baseUrl}/api/slot/create`;
+const slot = `${baseUrl}/api/slot`;
+const createZone = `${baseUrl}/api/zone/create`;
+const zone = `${baseUrl}/api/zone`;
+const allZone = `${baseUrl}/api/zone/all`;
 // Hubs 
-const allHubs=`${baseUrl}/api/hub/all`;
-const createHub =`${baseUrl}/api/hub/create`;
-const hub=`${baseUrl}/api/hub`;
+const allHubs = `${baseUrl}/api/hub/all`;
+const createHub = `${baseUrl}/api/hub/create`;
+const hub = `${baseUrl}/api/hub`;
 // Hub Mapping
-const hubMapping= `${baseUrl}/api/hub/mapping`;
+const hubMapping = `${baseUrl}/api/hub/mapping`;
 // Categories
-const categories=`${baseUrl}/api/category`;
+const categories = `${baseUrl}/api/category`;
 // Brands
-const Brands=`${baseUrl}/api/brand`;
-const allBrands=`${baseUrl}/api/brand/all`;
+const Brands = `${baseUrl}/api/brand`;
+const allBrands = `${baseUrl}/api/brand/all`;
 // Agents
-const agents=`${baseUrl}/api/agents/`;
+const agents = `${baseUrl}/api/agents/`;
 
 // pricing/types
-const pricingType =`${baseUrl}/api/pricing/types`;
+const pricingType = `${baseUrl}/api/pricing/types`;
 // Services
-const services=`${baseUrl}/api/services`;
-
-
+const services = `${baseUrl}/api/services`;
+// Orders
+const orders = `${baseUrl}/api/order/orders/all/`;
 
 export default {
   login,
@@ -52,4 +52,5 @@ export default {
   agents,
   pricingType,
   services,
+  orders,
 };
